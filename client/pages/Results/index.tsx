1ef7b5f9dcfe7a154d44298c430f3eaac6f2a8c5
Will Morgan <willmorgan@nuralogix.ai>
2025-07-25 16:50:53 -0400

    fix tabs code

--- client/pages/Results/index.tsx
+++ client/pages/Results/index.tsx
@@ -2,16 +2,17 @@ import state from '../../state';
 import { useSnapshot } from 'valtio';
 import ResultsError from './ResultsError';
 import ResultsSummary from './ResultsSummary';
+import { mockResults } from './mockResults';
 
 const Results = () => {
   const measurementSnap = useSnapshot(state.measurement);
   const { results } = measurementSnap;
 
-  if (!results) {
+  if (!mockResults) {
     return <ResultsError />;
   }
 
-  return <ResultsSummary results={results} />;
+  return <ResultsSummary results={mockResults} />;
 };
 
 export default Results;
