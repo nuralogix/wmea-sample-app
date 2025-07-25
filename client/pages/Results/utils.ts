7f74e987ae2b5967b2741f7a18960e951ef22e06
Will Morgan <willmorgan@nuralogix.ai>
2025-07-25 16:38:53 -0400

    cleanup

--- client/pages/Results/utils.ts
+++ client/pages/Results/utils.ts
@@ -1,4 +1,4 @@
-import type { Results, DfxPointId } from '@nuralogix.ai/web-measurement-embedded-app';
+import type { Results } from '@nuralogix.ai/web-measurement-embedded-app';
 
 // Utility: get all unique groups present in results.points
 export function getGroupsFromResults(points: Results['points']) {
@@ -12,9 +12,4 @@ export function getGroupsFromResults(points: Results['points']) {
 // Utility: get all points for a group
 export function getPointsForGroup(points: Results['points'], group: string) {
   return Object.entries(points).filter(([, pt]) => pt.meta.group === group);
-}
-
-// Utility: should render a group if at least one metric exists in results.points
-export function shouldRenderGroup(groupIds: DfxPointId[], points: Results['points']): boolean {
-  return groupIds.some((dfxPointId) => !!points[dfxPointId]);
 }
